591a41ca62d08f8063c288c580fd37e33db9d749
andrewshunt6@gmail.com <andrewshunt6@gmail.com>
2022-02-02 20:29:49 -0500

    change to ignore

---
+++ SPUC/Assets/AI/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy Stats")]
+    [SerializeField] int hp = 1;
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float attackSpeed = 1;
+    [SerializeField] float attackPower = 1;
+    [SerializeField] float defense = 0;
+
+    //[Header("Debuging")]
+    //[SerializeField] 
+    //GameObject target;
+
+    public void TakeDamage (float damage) {
+        hp -= Mathf.CeilToInt(damage - defense);
+        KillEnemy();
+    }
+
+    void KillEnemy(){
+        if (hp < 1){
+            Destroy(gameObject);
+        }
+    }
+}
