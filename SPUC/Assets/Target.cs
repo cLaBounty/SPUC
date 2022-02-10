67d72f9c9f2dc165ae1c6aca369bd7d11aa23110
RayZhengGit <60674449+RayZhengGit@users.noreply.github.com>
2022-02-09 21:07:19 -0500

    basic shooting

---
+++ SPUC/Assets/Target.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+// Source: https://www.youtube.com/watch?v=THnivyG0Mvo
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+	public void TakeDamage(float amount)
+	{
+		health -= amount;
+		if (health <= 0f)
+		{
+			die();
+		}
+	}
+
+	void die()
+	{
+		Destroy(gameObject);
+	}
+}
