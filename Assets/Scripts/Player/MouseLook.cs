ea5b0651714f21bc3b2a71e795a5ccfbd6e2fd71
cLaBounty <camlabounty@gmail.com>
2022-02-19 17:32:49 -0500

    Started working on Inventory screen toggle

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -20,14 +20,17 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-		xRotation -=mouseY;
-		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -=mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-		transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-		playerBody.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
 
 		//FOV increase for sprinting
         if (Input.GetKey(KeyCode.LeftShift)) 
