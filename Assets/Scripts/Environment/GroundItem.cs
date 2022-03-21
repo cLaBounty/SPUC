478873b979c5cebcb5dacf6e4a87c0f63892aedc
andrewshunt6@gmail.com <andrewshunt6@gmail.com>
2022-03-21 02:39:55 -0400

    small tweaks to sizes

--- Assets/Scripts/Environment/GroundItem.cs
+++ Assets/Scripts/Environment/GroundItem.cs
@@ -6,4 +6,19 @@ public class GroundItem : MonoBehaviour
 {
     public ItemObject item;
     public int amount = 1;
+    [SerializeField] float hoverRate = 1f;
+    [SerializeField] float highestOffset = 1f;
+    float startingZ;
+    float time = 0;
+
+    void Start() {
+        startingZ = transform.position.y;
+        time = Random.Range(0f, 1f);
+    }
+
+    void Update() {
+        time += hoverRate * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, startingZ + Mathf.Lerp(0, highestOffset, Mathf.Cos(time * Mathf.PI) * 0.5f + 0.5f), transform.position.z);
+    }
+
 }
