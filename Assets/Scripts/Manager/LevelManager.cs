064a819ded9825d915c9eab77ed706d3afefb48c
andrewshunt6@gmail.com <andrewshunt6@gmail.com>
2022-03-20 23:12:58 -0400

    adding in animations to crawler

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -11,11 +11,23 @@ public class LevelManager : MonoBehaviour
     GridController grid = null;
     Vector2 centerPos = Vector2.zero;
 
+    //enemy variables
+    OilDrill target;
+    GridController flowField;
+    PlayerMovement player;
+    Player playerStats;
+
 
     // Start is called before the first frame update
     void Start(){
         grid = GameObject.FindObjectOfType<GridController>();
         centerPos = (Vector2) grid.gridSize * grid.cellRadius;
+
+        target          = GameObject.FindObjectOfType<OilDrill>();
+        flowField       = GameObject.FindObjectOfType<GridController>();
+        player          = GameObject.FindObjectOfType<PlayerMovement>();
+        playerStats     = GameObject.FindObjectOfType<Player>();
+
         StartCoroutine(spawnEnemyTimer(timer));
     }
 
@@ -32,11 +44,19 @@ public class LevelManager : MonoBehaviour
             var inst = Instantiate(enemyPrefab);
             inst.transform.parent = null;
 
+        
+            //adjust values
+            //inst.target = target;
+            Enemy enemy         = inst.GetComponent<Enemy>();
+            enemy.target        = target;
+            enemy.flowField     = flowField;
+            enemy.playerStats   = playerStats;
+            enemy.player        = player;
+
             float randomDirection = Random.Range(0, 2 * Mathf.PI);
             inst.transform.position =   new Vector3(Mathf.Cos(randomDirection) * centerPos.x, 0, Mathf.Sin(randomDirection) * centerPos.y) +
                                         new Vector3(centerPos.x, 5, centerPos.y);
 
-            //inst.transform.position = new Vector3(inst.transform.position.x, 5f, inst.transform.position.y);
         }
     }
 }
