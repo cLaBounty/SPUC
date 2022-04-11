16ddcbe2733443d573ab757a6212e83bade3d8e9
Andrew Hunt <andrewshunt6@gmail.com>
2022-04-11 00:54:32 -0400

    enemy strt

---
+++ Assets/Scripts/Animations/EnemyFlyerAnimation1.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlyerAnimation1 : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] FlyingEnemy enemy;
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject projectileSpawnPoint;
+    int state;
+
+    bool attacking = false;
+    bool dying = false;
+    
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+
+    void LateUpdate() {
+
+        if (enemy.state == Enemy.STATE.DEAD )
+            animator.SetInteger("State", 2);
+
+        else if (enemy.state == Enemy.STATE.AGRO_OIL || enemy.state == Enemy.STATE.AGRO_PLAYER)//state < 4 && enemy.coolDown < 0){
+            animator.SetInteger("State", 0);
+        
+
+        else if (enemy.state == Enemy.STATE.ATTACKING_OIL || enemy.state == Enemy.STATE.ATTACKING_PLAYER)
+            animator.SetInteger("State", 1);
+        
+    }
+
+    public void AttackKeyFrame(){
+        enemy.DealDamage();
+    }
+
+    public void EndAttackKeyFrame(){
+        attacking = false;
+    }
+
+    public void Disapear(){
+        Destroy(enemy.gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyProjectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float projectileSpeed;
+    public GameObject target;
+    public Vector3 moveDirection;
+    [SerializeField] float steeringSpeed = 1f;
+
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null){
+            Vector3 directionToPlayer = target.transform.position - transform.position;
+            moveDirection = Vector3.RotateTowards(moveDirection, directionToPlayer, steeringSpeed * Time.deltaTime, 1f);
+            moveDirection.Normalize();
+        }
+
+        rb.velocity = moveDirection * projectileSpeed;
+    }
+}
