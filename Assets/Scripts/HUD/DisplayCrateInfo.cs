366095d7ea2a1231a694c13ddfdd2fff0d0b594a
andrewshunt6@gmail.com <andrewshunt6@gmail.com>
2022-05-04 17:58:04 -0400

    added new icons

--- Assets/Scripts/HUD/DisplayCrateInfo.cs
+++ Assets/Scripts/HUD/DisplayCrateInfo.cs
@@ -7,8 +7,18 @@ using TMPro;
 public class DisplayCrateInfo : MonoBehaviour
 {
     [SerializeField] private TMP_Text nameText;
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite commonSprite;
+    [SerializeField] private Sprite rareSprite;
+    [SerializeField] private Sprite epicSprite;
     
     public void SetUp(CrateRarity rarity) {
         nameText.text = rarity.ToString() + " Crate";
+
+        switch(rarity){
+            case CrateRarity.Common:    image.sprite = commonSprite; break;
+            case CrateRarity.Rare:      image.sprite = rareSprite; break;
+            case CrateRarity.Legendary: image.sprite = epicSprite; break;
+        }
     }
 }
